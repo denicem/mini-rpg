1012698d99a43923d7ea2446659bbaedd1ab7d00
Katarina Jovicic <katarina.jovicic@stud.hcw.ac.at>
2026-01-16 13:57:40 +0100

    Add images

---
+++ src/main/java/com/minirpg/game/util/Assets.java
@@ -0,0 +1,21 @@
+package com.minirpg.game.util;
+
+public final class Assets {
+
+    public Assets() {}
+
+    public static final String BG_CASTLE_INFRONT =
+            "castle_infront.png";
+
+    public static final String BG_CASTLE_INSIDE =
+            "castle_inside.png";
+
+    public static final String BG_FOREST =
+            "forest_no_combat.png";
+
+    public static String BG_DARK_FOREST =
+            "dark_forest.png.png";
+
+    public static String BG_AT_HOME =
+            "home.png";
+}
