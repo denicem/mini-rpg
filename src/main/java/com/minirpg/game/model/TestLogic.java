b92bcf692521519c71bf5ea9ad1e5faeab9b0d63
Katarina Jovicic <katarina.jovicic@stud.hcw.ac.at>
2026-01-16 13:57:40 +0100

    Add images

--- src/main/java/com/minirpg/game/model/TestLogic.java
+++ src/main/java/com/minirpg/game/model/TestLogic.java
@@ -1,5 +1,7 @@
 package com.minirpg.game.model;
 
+import com.minirpg.game.util.Assets;
+
 public class TestLogic {
     public static void main(String[] args) {
         System.out.println("Hellooo");
@@ -7,5 +9,8 @@ public class TestLogic {
 
         System.out.println(p.getName() + " is alive: " + p.isAlive());
         System.out.println(p.attack(p));
+
+        Assets assets = new Assets();
+        System.out.println(Assets.BG_CASTLE_INFRONT);
     }
 }
