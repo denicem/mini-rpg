11e2899ce3c27e623ed0f649ff3456bdaa402169
Vitoria Costa de Sousa <vitoria.costa.de.sousa@stud.hcw.ac.at>
2026-01-17 17:29:00 +0100

    docs for each class

--- src/main/java/com/minirpg/game/model/Potion.java
+++ src/main/java/com/minirpg/game/model/Potion.java
@@ -1,10 +1,21 @@
 package com.minirpg.game.model;
+/**
+ * Represents a Potion item in the MiniRPG game.
+ * When used, it restores 20 HP to the player, without exceeding the maximum HP.
+ */
 
 public class Potion implements Item{
+    //Returns the name of the item.
     @Override
     public String getName() {
         return "Potion";
     }
+
+
+    /**
+     * Uses the item on the player.
+     * Restores 20 HP, but not above the player's maximum HP.
+     */
 
     @Override
     public void use(Player p) {
--- src/main/java/com/minirpg/game/model/StrengthPotion.java
+++ src/main/java/com/minirpg/game/model/StrengthPotion.java
@@ -1,11 +1,23 @@
 package com.minirpg.game.model;
 
+/**
+ * Represents a Strength Potion item in the MiniRPG game.
+ * When used, it increases the player's attack by 5 points.
+ */
+
 public class StrengthPotion implements Item {
+
+    //Returns the name of the item.
 
     @Override
     public String getName() {
         return "Strength Potion";
     }
+
+    /**
+     * Uses the item on the player.
+     * Increases the player's attack (atk) by 5.
+     */
 
     @Override
     public void use(Player player) {
