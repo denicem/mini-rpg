d8e6abbb73a889bc183abe5cb25a4b7dcae263d2
Denice M <denicem@hotmail.de>
2026-01-18 03:46:07 +0100

    implement usage of items/inventory for class Player

--- src/main/java/com/minirpg/game/model/Player.java
+++ src/main/java/com/minirpg/game/model/Player.java
@@ -4,11 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player extends Character {
-    protected List<Item> items;
+    protected List<Item> inventory;
 
     public Player(String name) {
         super(name, 100, new Stats(10, 5));
-        this.items = new ArrayList<>();
+        this.inventory = new ArrayList<>();
+    }
+
+    public List<Item> getInventory() {
+        return inventory;
+    }
+
+    public String addItem(Item item) {
+        this.inventory.add(item);
+        return item.getName() + " added to inventory.";
+    }
+
+    public String useItem(Item item) {
+        if (this.inventory.contains(item)) {
+            item.use(this);
+            this.inventory.remove(item);
+
+            return this.getName() + " used " + item.getName();
+        }
+        return "Item does not exist.";
     }
 
     @Override
