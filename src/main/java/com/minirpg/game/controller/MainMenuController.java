25c88653da25a52be32bf09d47f8649d51964f69
Denice M <denicem@hotmail.de>
2026-01-20 12:28:04 +0100

    clean code MainMenu and implement logger to Helper class.

--- src/main/java/com/minirpg/game/controller/MainMenuController.java
+++ src/main/java/com/minirpg/game/controller/MainMenuController.java
@@ -1,28 +1,25 @@
 package com.minirpg.game.controller;
 
-import com.minirpg.game.util.GameSession;
-import com.minirpg.game.util.ViewManager;
 import javafx.fxml.FXML;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
 import com.minirpg.game.util.Assets;
-import javafx.stage.Stage;
+import com.minirpg.game.util.GameSession;
+import com.minirpg.game.util.Helper;
+import com.minirpg.game.util.ViewManager;
 
 public class MainMenuController {
-
-    @FXML
-    private ImageView backgroundImage;
-    @FXML private ImageView startBtnImage; // Neu für das Button-Bild
-    @FXML private ImageView exitBtnImage;  // Neu für das Button-Bild
+    @FXML private ImageView backgroundImage;
+    @FXML private ImageView startBtnImage;
+    @FXML private ImageView exitBtnImage;
 
     @FXML
     private void initialize() {
-        System.out.println(Assets.BG_START_SCREEN);
-        this.setView(Assets.BG_START_SCREEN);
+        Helper.logCall(this);
 
-        // Button-Grafiken laden
-        this.startBtnImage.setImage(new Image(getClass().getResourceAsStream(Assets.UI_START_BUTTON)));
-        this.exitBtnImage.setImage(new Image(getClass().getResourceAsStream(Assets.UI_EXIT_BUTTON)));
+        Helper.loadImage(backgroundImage, Assets.BG_START_SCREEN);
+        Helper.loadImage(startBtnImage, Assets.UI_START_BUTTON);
+        Helper.loadImage(exitBtnImage, Assets.UI_EXIT_BUTTON);
     }
 
     @FXML
@@ -34,20 +31,5 @@ public class MainMenuController {
     @FXML
     private void onExitClicked() {
         System.exit(0);
-    }
-
-    private void setView(String imagePath) {
-        try {
-            Image image = new Image(getClass().getResourceAsStream(imagePath));
-            this.backgroundImage.setImage(image);
-        } catch (Exception e) {
-            System.err.println("Error loading image: " + imagePath);
-            exitAndCloseWindow();
-        }
-    }
-
-    private void exitAndCloseWindow() {
-        Stage stage = (Stage) this.backgroundImage.getScene().getWindow();
-        stage.close();
     }
 }
